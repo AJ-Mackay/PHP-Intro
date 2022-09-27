eae87485c3df510c1b71757d2408313e2de6134d
Paul Mackay <paul.m.mackay85@gmail.com>
2022-09-27 15:34:44 +0100

    Variables in PHP

---
+++ variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$name = "Ash";
+$number = 100;
+$float = 100.5; // Floating Point number
+// PHP is case sensitive - "number", "Number" and "NUMBER" would all be seen as different variables.
+
+echo $name . " " . $number; // Full stops (.) are used for concatination
+
+$name = "<h1>HELLO!</h1>"; // Variables can have multiple values.
+
+echo $name;
+
+?>
+
+</body>
+</html>
