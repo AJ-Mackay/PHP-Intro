497134a54f968485716707bde4d643eef7fc0dd1
Paul Mackay <paul.m.mackay85@gmail.com>
2022-09-27 15:13:58 +0100

    Inserting Comments in PHP

---
+++ comments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+/* this is the first line
+this is the second line - multiline comments */
+
+
+//  This function below displays text - single line comments
+echo "Hello World";
+
+?>
+
+</body>
+</html>
