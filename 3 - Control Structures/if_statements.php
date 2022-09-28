be0cb42b20bd0155f6da575cc10986d8f063e68d
Paul Mackay <paul.m.mackay85@gmail.com>
2022-09-28 12:14:11 +0100

    If Statements

---
+++ 3 - Control Structures/if_statements.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+if(3 > 10){
+echo "Three is less than Ten";
+} elseif(4 < 5) {
+echo "of course Four is less than Five";
+} else {
+echo "it is not";
+}
+
+?>
+
+</body>
+</html>
