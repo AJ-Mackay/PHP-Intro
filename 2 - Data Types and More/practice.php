882a3cdf72a8f26f7da728bcff42c7a196d816ac
Paul Mackay <paul.m.mackay85@gmail.com>
2022-09-28 12:02:54 +0100

    tidy up

---
+++ 2 - Data Types and More/practice.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$number1 = 10;
+$number2 = 20;
+
+echo $number1 + $number2 . "<br>";
+
+$lists = array(23, 24, 25, 26, 27);
+$lists_assoc = array("number" => 10);
+
+echo $lists[0] . "<br>";
+echo $lists_assoc['number'];
+
+?>
+
+</body>
+</html>
