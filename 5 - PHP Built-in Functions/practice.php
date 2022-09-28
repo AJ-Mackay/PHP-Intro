98dec2ecd52d083a82b8c2c68fb4e58356f82bbe
Paul Mackay <paul.m.mackay85@gmail.com>
2022-09-28 16:14:09 +0100

    end module 5

---
+++ 5 - PHP Built-in Functions/practice.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+echo floor(3.7);
+echo "<br>";
+echo strtoupper("I love php!");
+echo "<br>";
+echo max([27, 4, 16]);
+
+?>
+</body>
+</html>
