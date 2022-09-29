207b6f016997622319718111ab1a3734b82bf4d0
Paul Mackay <paul.m.mackay85@gmail.com>
2022-09-29 14:09:38 +0100

    Validating The Form Values

--- 6 - How to use Form Data in PHP/form.php
+++ 6 - How to use Form Data in PHP/form.php
@@ -1,13 +1,31 @@
 <?php
 
-if(isset($_POST['submit'])){}
+if(isset($_POST['submit'])){
+
+$name = array("Edwin", "Student", "Peter", "Samid", "Mohad", "Maria", "Jane");
+$minimum = 5;
+$maximum = 10;
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-echo "Hello " . $username . "<br>";
-echo "Your password is " . $password;
+if(strlen($username) < $minimum){
+echo "Username has to be longer than five characters";
+}
 
+if(strlen($username) > $maximum){
+echo "Username cannot be longer than ten characters";
+}
+
+if(!in_array($username, $name)){
+echo "Sorry, you are not allowed";
+} else {
+echo "Welcome " . $username;  
+}
+
+// echo "Hello " . $username . "<br>";
+// echo "Your password is " . $password;
+}
 ?>
 
 <!DOCTYPE html>
