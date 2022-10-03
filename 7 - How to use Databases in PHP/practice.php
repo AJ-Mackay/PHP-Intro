f0d3c1c7c2dce1f71432f5e26a221fca78b5169b
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-03 17:17:46 +0100

    End module 7

---
+++ 7 - How to use Databases in PHP/practice.php
@@ -0,0 +1,38 @@
+<?php 
+
+$connection = mysqli_connect('localhost', 'root', '', 'mylist');
+if(!$connection) {
+    die("Database Connection Failed!");
+} else {
+    echo "Connected!";
+}
+
+$query = "SELECT * FROM reports";
+
+$result = mysqli_query($connection, $query);
+
+if(!$result) {
+    die("Query Failed!");
+}
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+while($record = mysqli_fetch_assoc($result)){
+    // print_r($record);
+    echo $record['days'] . "<br>";
+}
+
+?>
+</body>
+</html>
