d289460eee2c14a2005fa1f50ede3054261f8b0b
Paul Mackay <paul.m.mackay85@gmail.com>
2022-10-04 15:35:00 +0100

    How to create Class Methods in PHP

--- 10 - Object Oriented PHP Introduction/class_car.php
+++ 10 - Object Oriented PHP Introduction/class_car.php
@@ -2,13 +2,22 @@
 
 class Car {
 
+    function MoveWheels(){
+        echo "Wheels move";
+    }
+
 }
 
 if(class_exists("Car")) {
-    echo "Class Car already exists";
+    echo "Class Car exists" . "<br>";
 } else {
-    echo "No!";
+    echo "No Class found!" . "<br>";
 }
 
+if(method_exists("Car", "MoveWheels")) {
+    echo "The MoveWheels Method exists" . "<br>";
+} else {
+    echo "No Method found!" . "<br>";
+}
 
 ?>
